37d706ba551b31abdd66b9ac3b3e76a062e8c3fb
Juan Manuel Gómez Cobo <jgomezcobo@uade.edu.ar>
2023-11-09 09:51:59 -0300

    Maze Update
    • Added maze
    • Added background parallax

--- Scripts/playerController.gd
+++ Scripts/playerController.gd
@@ -1,6 +1,6 @@
 extends RigidBody2D
 
-var force = 20
+var force = 1000
 
 func _physics_process(delta):
 	
@@ -11,7 +11,7 @@ func _physics_process(delta):
 		apply_force(Vector2(-force, 0))
 		
 	if Input.is_action_pressed("move_up"):
-		apply_force(Vector2(0, force))
+		apply_force(Vector2(0, -force))
 		
 	if Input.is_action_pressed("move_down"):
-		apply_force(Vector2(0, -force))
+		apply_force(Vector2(0, force))
